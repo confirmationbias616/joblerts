8753c1dd6a1b626f3bbf054dfd74708ef9ddb85b
Alex Roy <confirmationbias616>
2020-02-16 21:22:26 -0500

    log search results to db and fix looping

--- routine.py
+++ routine.py
@@ -11,10 +11,18 @@ with create_connection() as conn:
 
 def show_results():
     for _, row in search_df.iterrows():
-        result = get_posting_links(row['career_page'], row.keywords)[0]
-        title = re.findall('[A-z0-9 \-\/:]+', result[1].title())[0].lstrip().rstrip()
-        link = result[0]
-        return f"{title}: {link}"
+        results = get_posting_links(row['career_page'], row.keywords)
+        if results:
+            result = results[0]
+            title = re.findall('[A-z0-9 \-\/:]+', result[1].title())[0].lstrip().rstrip()
+            link = result[0]
+            print(f"{title}: {link}")
+            with create_connection() as conn:
+                conn.cursor().execute(f"""
+                INSERT INTO found (search_id, link, date_found) VALUES (?, ?, ?)
+            """, [row.id, link, datetime.datetime.now().date()])
+        else:
+            print("nothing found.")
 
 if __name__ == "__main__":
-    print(show_results())
+    show_results()
