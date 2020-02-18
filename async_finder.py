f9146bb14f1c9c0b05d9431c128c10f39f71f312
Alex Roy <confirmationbias616>
2020-02-17 20:37:47 -0500

    Attempt to weed out more bad results

--- async_finder.py
+++ async_finder.py
@@ -1,17 +1,24 @@
 import asyncio
 import aiohttp
 from bs4 import BeautifulSoup
+import re
 
 
 async def main():
     async def get_content(url):
+        if '.' not in url:
+            url = re.findall('.*(?=/)', search_url)[0] + url
         try:
-            print(f'trying {url}...')
+            print(f'trying {url} ...')
             async with aiohttp.ClientSession() as session:
                 async with session.get(url, headers={'User-Agent': 'Mozilla/5.0'}) as response:
-                    url_content = str(await response.read())
+                    print(response.status)
+                    url_content = ''
+                    if response.status == 200:
+                        url_content = str(await response.read())
             return url_content
-        except (TypeError, aiohttp.client_exceptions.InvalidURL):
+        except (TypeError, aiohttp.client_exceptions.InvalidURL, aiohttp.client_exceptions.ClientConnectorError):
+            print(f"{url} didn't work")
             return ''
 
     async def task(work_queue):
@@ -22,29 +29,38 @@ async def main():
                 async with session.get(url, headers={'User-Agent': 'Mozilla/5.0'}) as response:
                     content = await response.read()
                     soup = BeautifulSoup(str(content), "html.parser")
-                    postings = {posting:await get_content(posting.get('href')) for posting in soup.find_all("a")}
-                    print(f"???{len(postings)}")
+                    postings = {}
+                    for posting in soup.find_all("a"):
+                        if posting.get('href'):
+                            posting_content = await get_content(posting.get('href'))
+                            posting_soup = BeautifulSoup(posting_content, 'html.parser')
+                            posting_sentences = re.findall("([^\.!?\'\"&:=|%]{20,})", str(posting_soup.find_all(text=True)))
+                            posting_text = ' '.join(posting_sentences).lower()
+                            if len(posting_sentences) > 10:
+                                for career_term in ('responsibilities', 'full-time', 'salary', 'posting', 'equal opportunity', 'full time', 'part-time', 'part time'):
+                                    if career_term in posting_text:
+                                        postings.update({posting:posting_text})
+                                        break
                     for search in page_search:
-                        print(f"searching for: {search}")
                         search_id = search[0]
-                        keywords = search[1]
-                        good_posting_links = {posting.get('href'): posting.get_text() for posting in postings if keywords in postings[posting].lower()}
-                        if good_posting_links:
-                            print(f"WOOH OOOO: {good_posting_links}")
-                            results.append([search_id, good_posting_links])
+                        keywords = search[1].lower()
+                        matched_postings = {posting.get('href'): posting.get_text() for posting in postings if keywords in postings[posting].lower()}
+                        if matched_postings:
+                            print(f"WOOH OOOO: {matched_postings}")
+                            results.append([search_id, matched_postings])
 
     results = []
     page_searches = {
-        "https://www.shopify.ca/careers/search?keywords=&sort=specialty_asc":[['1','the'], ['7','a']],
-        "https://jobs.lever.co/close.io/":[['2','bla'], ['8','block']],
-        "https://lixar.com/careers/":[['3','blab'], ['9','blab']],
-        "https://journey.buffer.com/#vacancies":[['4','thought'], ['10','thought']],
-        "https://www.shopify.ca/careers/search":[['5','face'], ['11','face']],
-        "https://www.klipfolio.com/careers":[['6','hit'], ['12','log']],
+        "https://www.shopify.ca/careers/search?keywords=&sort=specialty_asc":[['1','Marketing']],
+        "https://jobs.lever.co/close.io/":[['2','Marketing']],
+        "https://lixar.com/careers/":[['3','Marketing']],
+        "https://journey.buffer.com/#vacancies":[['4','Marketing']],
+        "https://www.shopify.ca/careers/search":[['5','Marketing']],
+        "https://www.klipfolio.com/careers":[['6','front-end']],
     }
     work_queue = asyncio.Queue()
-    for url in page_searches:
-        await work_queue.put(url)
+    for search_url in page_searches:
+        await work_queue.put(search_url)
     await asyncio.gather(
         asyncio.create_task(task(work_queue)),
         asyncio.create_task(task(work_queue)),
@@ -71,6 +87,7 @@ async def main():
         asyncio.create_task(task(work_queue)),
         asyncio.create_task(task(work_queue)),
     )
+    import pdb; pdb.set_trace()
     print(results)
 
 if __name__ == "__main__":
