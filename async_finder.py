837c0c4811d305fd2ebdb808e683709d3d1feb35
Alex Roy <confirmationbias616>
2020-02-18 23:25:29 -0500

    Fix issues with async and replace as default finder process

--- async_finder.py
+++ async_finder.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup
 import re
 
 
-async def main():
+async def main(page_searches):
     async def get_content(url):
         if '.' not in url:
             url = re.findall('.*(?=/)', search_url)[0] + url
@@ -24,40 +24,61 @@ async def main():
     async def task(work_queue):
         async with aiohttp.ClientSession() as session:
             while not work_queue.empty():
-                url = await work_queue.get()
-                page_search = page_searches[url]
-                async with session.get(url, headers={'User-Agent': 'Mozilla/5.0'}) as response:
+                search_url = await work_queue.get()
+                page_search = page_searches[search_url]
+                parent_url = re.findall('.*/(?=.*)', search_url.rstrip('/'))[0]
+                async with session.get(parent_url, headers={'User-Agent': 'Mozilla/5.0'}) as response:
+                    parent_content = await response.read()
+                soup = BeautifulSoup(str(parent_content), "html.parser")
+                irrelevant_links = [a.get('href') for a in soup.find_all('a')]
+                async with session.get(search_url, headers={'User-Agent': 'Mozilla/5.0'}) as response:
                     content = await response.read()
                     soup = BeautifulSoup(str(content), "html.parser")
                     postings = {}
                     for posting in soup.find_all("a"):
                         if posting.get('href'):
+                            if posting.get('href') in irrelevant_links:
+                                continue
                             posting_content = await get_content(posting.get('href'))
+                            if not posting_content:
+                                continue
                             posting_soup = BeautifulSoup(posting_content, 'html.parser')
-                            posting_sentences = re.findall("([^\.!?\'\"&:=|%]{20,})", str(posting_soup.find_all(text=True)))
-                            posting_text = ' '.join(posting_sentences).lower()
-                            if len(posting_sentences) > 10:
-                                for career_term in ('responsibilities', 'full-time', 'salary', 'posting', 'equal opportunity', 'full time', 'part-time', 'part time'):
-                                    if career_term in posting_text:
-                                        postings.update({posting:posting_text})
-                                        break
+                            postings.update({posting:posting_soup})
                     for search in page_search:
                         search_id = search[0]
                         keywords = search[1].lower()
-                        matched_postings = {posting.get('href'): posting.get_text() for posting in postings if keywords in postings[posting].lower()}
+                        matched_postings = {}
+                        for posting in postings:
+                            print(f"Maybe {posting.get_text()}?")
+                            posting_text = ' '.join(postings[posting].find_all(text=True))
+                            if not keywords in posting_text.lower():
+                                print(f"Nope, didn't contain keyword {keywords}")
+                                continue
+                            posting_sentences = re.findall("([A-Z][^\.!?\'\"&:=|%{}]{60,}[\.!?])", posting_text)
+                            if len(posting_sentences) <= 13:
+                                print(f"Nope, didn't contain enough sentences ({len(posting_sentences)})")
+                                print(posting)
+                                continue
+                            for career_term in ('responsibilities', 'full-time', 'salary', 'posting', 'equal opportunity', 'full time', 'part-time', 'part time'):
+                                if career_term in posting_text.lower():
+                                    matched_postings.update({posting.get('href'): posting.get_text()})
+                                    print(f"Yes, it's a match for {posting.get_text()}! -> contains {len(posting_sentences)} sentences and career term `{career_term}`")
+                                    break
+                            else:
+                                print(f"Nope, didn't contain a career term")
                         if matched_postings:
                             print(f"WOOH OOOO: {matched_postings}")
                             results.append([search_id, matched_postings])
 
     results = []
-    page_searches = {
-        "https://www.shopify.ca/careers/search?keywords=&sort=specialty_asc":[['1','Marketing']],
-        "https://jobs.lever.co/close.io/":[['2','Marketing']],
-        "https://lixar.com/careers/":[['3','Marketing']],
-        "https://journey.buffer.com/#vacancies":[['4','Marketing']],
-        "https://www.shopify.ca/careers/search":[['5','Marketing']],
-        "https://www.klipfolio.com/careers":[['6','front-end']],
-    }
+    # page_searches = {
+    #     "https://www.shopify.ca/careers/search?keywords=&sort=specialty_asc":[['1','Marketing']],
+    #     "https://jobs.lever.co/close.io/":[['2','Marketing']],
+    #     "https://lixar.com/careers/":[['3','Marketing']],
+    #     "https://journey.buffer.com/#vacancies":[['4','Marketing']],
+    #     "https://www.shopify.ca/careers/search":[['5','Marketing']],
+    #     "https://www.klipfolio.com/careers":[['6','front-end']],
+    # }
     work_queue = asyncio.Queue()
     for search_url in page_searches:
         await work_queue.put(search_url)
@@ -87,8 +108,10 @@ async def main():
         asyncio.create_task(task(work_queue)),
         asyncio.create_task(task(work_queue)),
     )
-    import pdb; pdb.set_trace()
-    print(results)
+    for result in results:
+        for posting in result[1]:
+            print(f"{result[1][posting]}: {posting}")
+    return results
 
 if __name__ == "__main__":
     asyncio.run(main())
--- routine.py
+++ routine.py
@@ -1,28 +1,28 @@
 import pandas as pd
 import datetime
 import re
+import asyncio
 
 from finder import get_posting_links
+from async_finder import main
 from utils import create_connection
 
 
-with create_connection() as conn:
-    search_df = pd.read_sql("SELECT * FROM search", conn)
-
-def show_results():
+async def routine():
+    with create_connection() as conn:
+        search_df = pd.read_sql("SELECT * FROM search", conn)
+    page_searches = {}
     for _, row in search_df.iterrows():
-        results = get_posting_links(row['career_page'], row.keywords)
-        if results:
-            result = results[0]
-            title = re.findall('[A-z0-9 \-\/:]+', result[1].title())[0].lstrip().rstrip()
-            link = result[0]
-            print(f"{title}: {link}")
+        page_searches.update({row.career_page: [[str(row.id), row.keywords]]})
+    results = await main(page_searches)
+    for result in results:
+        search_id = result[0]
+        for posting in result[1]:
+            print(f"{result[1][posting]}: {posting}")
             with create_connection() as conn:
                 conn.cursor().execute(f"""
-                INSERT INTO found (search_id, link, date_found) VALUES (?, ?, ?)
-            """, [row.id, link, datetime.datetime.now().date()])
-        else:
-            print("nothing found.")
+                    INSERT INTO found (search_id, link, keywords, date_found) VALUES (?, ?, ?, ?)
+                """, [search_id, posting, result[1][posting], datetime.datetime.now().date()])
 
 if __name__ == "__main__":
-    show_results()
+    asyncio.run(routine())
